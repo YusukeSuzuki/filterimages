4b6d1df64eb228e9fa085e526c286837ced65823
Yusuke Suzuki <yusuke@geekfield.jp>
2017-03-20 17:39:36 +0900

    add width/height conditions

--- filterimages/main.py
+++ filterimages/main.py
@@ -9,11 +9,19 @@ def proc(pathstr, args):
         rgb_arr = np.array(image, dtype=np.float32)
         hsv_arr = np.array(image.convert('HSV'), dtype=np.float32)
         s_arr = hsv_arr.transpose([2,0,1])[1].reshape([-1])
-        #print("saturation: avg = ", np.average(s_arr), ", sdv = ", np.std(s_arr))
 
         ok = True
 
         sat_avg = 0.
+
+        if args.width_min is not None and args.width_min > image.width:
+            ok = False
+        if args.width_min is not None and args.width_max < image.width:
+            ok = False
+        if args.height_min is not None and args.height_min > image.height:
+            ok = False
+        if args.height_min is not None and args.height_max < image.height:
+            ok = False
 
         if args.sat_avg_min is not None or args.sat_avg_max is not None:
             sat_avg = np.average(s_arr)
@@ -49,6 +57,11 @@ def run():
 
     parser.add_argument("--sat-avg-min", type=float, default=None)
     parser.add_argument("--sat-avg-max", type=float, default=None)
+
+    parser.add_argument("--width-min", type=float, default=None)
+    parser.add_argument("--width-max", type=float, default=None)
+    parser.add_argument("--height-min", type=float, default=None)
+    parser.add_argument("--height-max", type=float, default=None)
 
     parser.add_argument("--aspect-min", type=float, default=None)
     parser.add_argument("--aspect-max", type=float, default=None)
